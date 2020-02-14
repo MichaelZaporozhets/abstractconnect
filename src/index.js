f9a75cbc60254bb36168a65f026161ae8a13f1bf
Michael Zaporozhets <michaelzaporozhets@gmail.com>
2020-02-14 11:05:06 +1100

    ta-da

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import fs from 'fs';
 import { client } from './client';
 import { ABSTRACT_PROJECT_ID, ABSTRACT_BRANCH, ABSTRACT_FILE_NAME, TEMP_PATH } from './constants';
 import { doLog } from './log';
+import { Sketch } from 'sketch-constructor';
+import { cleanSymbol } from './sketchCleaners';
 
 async function getMasterHead() {
   const masterBranchInfo = await client.branches.info({
@@ -13,18 +15,18 @@ async function getMasterHead() {
   return masterBranchInfo.head;
 }
 
-async function getProjectFiles() {
+async function getProjectFiles(sha) {
   const files = await client.files.list({
     branchId: ABSTRACT_BRANCH,
     projectId: ABSTRACT_PROJECT_ID,
-    sha: "latest"
+    sha: sha || "latest"
   });
 
   return files;
 }
 
-async function getFileByName(name) {
-  const files = await getProjectFiles();
+async function getFileData(name, sha) {
+  const files = await getProjectFiles(sha);
   return files.find(file => file.name === name);
 }
 
@@ -39,15 +41,48 @@ async function downloadRawFile(file, outputPath) {
   });
 }
 
-async function getLatestAsSketchFile() {
-  const file = await getFileByName(ABSTRACT_FILE_NAME);
-
+async function getSketchFile(fromSHA) {
+  const file = await getFileData(ABSTRACT_FILE_NAME, fromSHA);
   const outputPath = `${TEMP_PATH}/${file.sha}.sketch`;
+  const fileExists = fs.existsSync(outputPath);
 
-  if(!fs.existsSync(outputPath)) {
+  if(!fileExists) {
     doLog('\nabstractconnect: downloading data... ==\n');
     const result = await downloadRawFile(file, outputPath);
   }
+
+  return outputPath;
 }
 
-getLatestAsSketchFile().then(console.log).catch(console.error);
+async function getLocalSketchFileBySHA(sha) {
+  const path = `${TEMP_PATH}/${sha}.sketch`;
+  return path;
+}
+
+async function getSymbols({ raw, withFilter, fromSHA }) {
+  const sketchFile = await getSketchFile(fromSHA);
+  const sketchFileData = await Sketch.fromFile(sketchFile);
+
+  // sketch creates a special page for all the symbols
+  const allSymbols = sketchFileData.pages.find(page => page.name === "Symbols");
+
+  // they're read as layers though
+  const ensuredSymbols = allSymbols.layers.filter(symbolLayer => symbolLayer._class === "symbolMaster")
+
+  // apply filter if passed
+  const filteredSymbols = withFilter ? ensuredSymbols.filter(withFilter) : ensuredSymbols;
+
+  // clean up the obj model by default
+  const refined = raw === true ? filteredSymbols : filteredSymbols.map(cleanSymbol);
+
+  // send it back!
+  return refined;
+}
+
+export {
+  getMasterHead,
+  getProjectFiles,
+  getFileData,
+  downloadRawFile,
+  getSymbols
+}
---
+++ src/sketchCleaners.js
@@ -0,0 +1,121 @@
+function cleanColor({ red, green, blue, alpha }) {
+  return `rgba(${parseInt(red.toFixed(5)*255)}, ${parseInt(green.toFixed(5)*255)}, ${parseInt(blue.toFixed(5)*255)}, ${alpha})`
+}
+
+function hasText(layer) {
+  return layer.attributedString && true;
+}
+
+function cleanText(textStyle) {
+  return {
+    color: cleanColor(textStyle.encodedAttributes.MSAttributedStringColorAttribute),
+    fontSize: textStyle.encodedAttributes.MSAttributedStringFontAttribute.attributes.size,
+    fontFamily: textStyle.encodedAttributes.MSAttributedStringFontAttribute.attributes.name,
+    lineHeight: textStyle.encodedAttributes.paragraphStyle.maximumLineHeight,
+    alignment: textStyle.encodedAttributes.paragraphStyle.alignment,
+    verticalAlignment: textStyle.verticalAlignment
+  }
+}
+
+function cleanGradient(gradient) {
+  return {
+    from: gradient.from,
+    to: gradient.to,
+    stops: gradient.stops.map(stop => ({ color: cleanColor(stop.color) })),
+  }
+}
+
+function hasBackground(layerStyle) {
+  return layerStyle.fills.length > 0;
+}
+
+function cleanBackground(background) {
+  return {
+    fill: cleanColor(background.color),
+    hasGradient: background.fillType == 1,
+    gradient: background.fillType == 1 && cleanGradient(background.gradient)
+  }
+}
+
+function cleanBorder(border) {
+  return {
+    color: cleanColor(border.color),
+    width: border.thickness
+  }
+}
+
+function hasBorder(layerStyle) {
+  return layerStyle.borders.length && layerStyle.borders[0].isEnabled
+}
+
+function cleanLayer(layer) {
+  console.log(layer.style)
+  return {
+    // loves a good name
+    name: layer.name,
+
+    // it's useful to know what type of layer it is
+    type: layer._class,
+
+    // if it's a text layer, we'll probably need the text in the layer
+    hasText: hasText(layer),
+    text: hasText(layer) && layer.attributedString.string,
+
+    // some useful points of data
+    isVisible: layer.isVisible,
+    isLocked: layer.isLocked,
+
+    // shapes are useful to know!
+    isShape: layer.points && layer.points.length > 0,
+    shapeData: layer.points,
+
+    // is this layer a parent?
+    hasChildren: layer.layers && layer.layers.length > 0,
+    children: layer.layers && layer.layers.map(cleanLayer),
+
+    // the big one!
+    style: {
+      width: layer.frame.width,
+      height: layer.frame.height,
+      top: layer.frame.x,
+      left: layer.frame.y,
+      text: hasText(layer) && cleanText(layer.style.textStyle),
+      background: hasBackground(layer.style) && cleanBackground(layer.style.fills[0]),
+      border: hasBorder(layer.style) && cleanBorder(layer.style.borders[0]),
+      shadows: {
+        outer: layer.style.shadows,
+        inner: layer.style.innerShadows
+      }
+    }
+  }
+}
+
+function cleanSymbol(symbol) {
+  return {
+    name: symbol.name,
+    layers: symbol.layers.map(cleanLayer),
+
+    dimensions: {
+      width: symbol.frame.width,
+      height: symbol.frame.height
+    },
+
+    position: {
+      x: symbol.frame.x,
+      y: symbol.frame.y
+    }
+  };
+}
+
+export {
+  cleanColor,
+  cleanBorder,
+  cleanGradient,
+  cleanBackground,
+  cleanText,
+  cleanLayer,
+  cleanSymbol,
+  hasText,
+  hasBorder,
+  hasBackground
+}
